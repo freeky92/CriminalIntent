ede6b84e729ebc77c4ea45d1d44cf69d934ba2c5
Vladimir Lari4ev <freeky92s@gmail.com>
2022-04-09 16:41:36 +0300

    09.04.2022

--- app/src/main/java/com/asurspace/criminalintent/domain/usecase/get/GetCrimesListUseCase.kt
+++ app/src/main/java/com/asurspace/criminalintent/domain/usecase/get/GetCrimesListUseCase.kt
@@ -1,6 +1,14 @@
 package com.asurspace.criminalintent.domain.usecase.get
 
+import com.asurspace.criminalintent.domain.repository.GetAddUpdateCrimeRepository
+import com.asurspace.criminalintent.model.crimes.entities.Crime
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCrimesListUseCase @Inject constructor(){
+class GetCrimesListUseCase @Inject constructor(
+    private val getAddUpdateCrimeRepository: GetAddUpdateCrimeRepository
+){
+    operator fun invoke(): Flow<MutableList<Crime>> {
+        return getAddUpdateCrimeRepository.getAllCrimes()
+    }
 }
